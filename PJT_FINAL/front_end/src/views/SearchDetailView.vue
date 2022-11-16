516eece52d616a6ed6423946517982ee59360d43
mihyeon1234 <algus7531@naver.com>
2022-11-16 10:45:24 +0900

    detail div

---
+++ PJT_FINAL/front_end/src/views/SearchDetailView.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="SearchContainer">
+        <DetailVue/>
+    </div>
+
+</template>
+
+<script>
+import DetailVue from '@/components/search/SearchDetail.vue';
+export default {
+    name:'SearchDetailView',
+    components:{
+        DetailVue,
+    }
+}
+</script>
+
+<style>
+.SearchContainer{
+    margin-left: 50px;
+    height: 100vh;
+}
+
+</style>
