b728d1a3227e1b770233c5109b0397694c92c7c4
mihyeon1234 <algus7531@naver.com>
2022-11-21 14:53:34 +0900

    chart

--- PJT_FINAL/front_end/src/components/chart/cardDiv.vue
+++ PJT_FINAL/front_end/src/components/chart/cardDiv.vue
@@ -1,33 +1,48 @@
 <template>
   <div>
-    <carousel-3d class="carousel">
-      <slide v-for="aa in 5" :key="aa" :index="aa-1">
-        <img alt="Vue logo" :src="link_url" />
+
+    <carousel-3d :controls-visible="true"  :controls-prev-html="'&#10092; '" :controls-next-html="'&#10093;'" :controls-width="30" :controls-height="60" :clickable="false"  :width="500" :height="750" class="carousel">
+      <slide v-for="(recommendmovie, index) in recommendmovies" :key="recommendmovie" :index="index-1">
+        <ChartRecommendCard :recommendmovie="recommendmovie" />
       </slide>
     </carousel-3d>
+    <div>
+    </div>
   </div>
+  
 </template>
 
 <script>
 import { Carousel3d, Slide } from "vue-carousel-3d";
-
+import ChartRecommendCard from './cardImg.vue'
 export default {
   name: "LankIndexCard",
   data() {
     return {
-        link_url: `https://search.pstatic.net/sunny/?src=https%3A%2F%2Fdnvefa72aowie.cloudfront.net%2Forigin%2Farticle%2F202206%2FA03D68CD75227C1C7E7A0F6E8D7AEC59FD7697ED2A0BB8B57F8CFD4E0F0B1B02.jpg%3Fq%3D95%26s%3D1440x1440%26t%3Dinside&type=sc960_832`,
-    };
+      link_url: `https://image.tmdb.org/t/p/original/${this.recommendmovies}`,
+
+      };
   },
   props:{
-
+    recommendmovies:Array
   },
   components: {
     Carousel3d,
-    Slide
+    Slide,
+    ChartRecommendCard
+  },
+  create(){
+    console.log(this.recommendmovies);
   }
 
 };
 </script>
 
 <style>
+
+#app > div > div > div.LankCardDiv > div > div > div.carousel-3d-container.carousel > div.carousel-3d-slider > div.carousel-3d-slide
+
+{
+  background-color: transparent;
+}
 </style>
--- PJT_FINAL/front_end/src/components/chart/naverApi.py
+++ PJT_FINAL/front_end/src/components/chart/naverApi.py
@@ -1,6 +1,6 @@
-import requests
 from bs4 import BeautifulSoup
 from datetime import datetime
+import requests
 
 lanktoday = datetime.today().strftime('%Y%m%d')
 headers = {'User-Agent' : 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36'}
@@ -12,39 +12,44 @@ movies = soup.select("#old_content > table > tbody > tr")
 movie_list=[]
 for movie in movies:
     movie_name = movie.select_one("td.title > div > a")
+    # movie_href = movie_name.get_attribute('href')
+    print(movie_name)
+    # print(movie_href)
     # movie_point = movie.select_one("td.point")
     if movie_name is not None:
         if len(movie_list)<10:
             if movie.select_one("td:nth-child(1) > img"):
                 ranking = movie.select_one("td:nth-child(1) > img")["alt"]
-                movie_list.append([int(ranking), movie_name.text])
+                movie_list.append([int(ranking), movie_name.text, movie_name.href])
+                # print(movie.select_one)
             else:
                 ranking = len(movie_list)
-                movie_list.append([int(ranking), movie_name.text])
+                movie_list.append([int(ranking), movie_name.text, movie_name.href])
         else:break
-
 # print(movie_list)
 
 
-import os
-import sys
-import urllib.request
-client_id = "zp580fkHgE57jjytcezD"
-client_secret = "opbueu7dbW"
-for movie in movie_list:
-    encText = urllib.parse.quote(movie[1])
-    url = "https://openapi.naver.com/v1/search/movie.json?query=" + encText # JSON 결과
-    # url = "https://openapi.naver.com/v1/search/blog.xml?query=" + encText # XML 결과
-    request = urllib.request.Request(url)
-    request.add_header("X-Naver-Client-Id",'zp580fkHgE57jjytcezD')
-    request.add_header("X-Naver-Client-Secret",'opbueu7dbW')
-    response = urllib.request.urlopen(request)
-    rescode = response.getcode()
-    if(rescode==200):
-        response_body = response.read()
-        print(response_body.decode('utf-8'))
-    else:
-        print("Error Code:" + rescode)
+# import os
+# import sys
+# import json
+# import urllib.request
+# client_id = "zp580fkHgE57jjytcezD"
+# client_secret = "opbueu7dbW"
+# # for movie in movie_list:
+# encText = urllib.parse.quote('리멤버')
+# url = "https://openapi.naver.com/v1/search/movie.xml?query=%EC%A3%BC%EC%8B%9D&display=10&start=1&genre=1" # JSON 결과
+# # url = "https://openapi.naver.com/v1/search/blog.xml?query=" + encText # XML 결과
+# request = urllib.request.Request(url)
+# request.add_header("X-Naver-Client-Id",'zp580fkHgE57jjytcezD')
+# request.add_header("X-Naver-Client-Secret",'opbueu7dbW')
+# response = urllib.request.urlopen(request)
+# rescode = response.getcode()
+# if(rescode==200):
+#     response_body = response.read()
+#     test1=json.loads(response_body.decode('utf-8'))
+#     print(test1.get('items'))
+# else:
+#     print("Error Code:" + rescode)
 
 # 받아온 데이터로 카드 만들고, 카드 눌렀을때 아이프레임으로(items.link) 모달 띄우면 이쁠듯
 # link 받아온 거에서 백슬러쉬 없애고 src에 보내줘야됨
